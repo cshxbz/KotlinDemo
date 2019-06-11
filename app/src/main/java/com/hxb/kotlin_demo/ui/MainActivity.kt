93562381fc59f8d20c592e0c71dd142c98db6c3f
cshxbz <cshxbz@163.com>
2019-06-11 14:27:24 +0800

    some commit

--- app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
+++ app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
@@ -2,7 +2,10 @@ package com.hxb.kotlin_demo.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
 import com.hxb.kotlin_demo.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+         * 导入 kotlinx.android.synthetic.main.＜布局＞.* 后，
+         * 可以免去 findViewById, 可直接访问布局中的控件，使用控件的id来访问
+         */
+        tv_text.setOnClickListener { v: View ->
+            println("clicked ${(v as TextView).text}")
+        }
     }
 }
