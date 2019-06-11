13d68f8bd87177b108a2eb830df8c833d43690a6
cshxbz <cshxbz@163.com>
2019-06-11 11:03:46 +0800

    some commit

--- app/src/main/java/com/hxb/kotlin_demo/GenericSample.kt
+++ app/src/main/java/com/hxb/kotlin_demo/GenericSample.kt
@@ -17,12 +17,32 @@ interface Comparable<in T>{
 }
 
 
+interface Tag<T>{
+    fun getContent(): T
+    fun setContent(content: T)
+}
+
+
+fun genericTest1(tag: Tag<String>) {
+    val other: Tag<out Any> = tag
+
+    //编译出错误
+//    other.setContent(1)
+
+    other.getContent()
+}
+
+fun genericTest2(tag: Tag<Number>) {
+    val other: Tag<in Int> = tag
+    other.setContent(10)
+    other.getContent()
+}
+
 fun genericOutTest(strs: Source<String>) {
 
     val objs: Source<Any> = strs
 
 }
-
 
 fun genericInTest(x: Comparable<Number>) {
 
