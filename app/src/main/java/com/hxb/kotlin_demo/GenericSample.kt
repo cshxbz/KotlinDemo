adf3dd813ab1e6af004a78e52ccbcd93a6bfd3a4
cshxbz <cshxbz@163.com>
2019-07-05 15:50:13 +0800

    some commit

--- app/src/main/java/com/hxb/kotlin_demo/GenericSample.kt
+++ app/src/main/java/com/hxb/kotlin_demo/GenericSample.kt
@@ -1,5 +1,8 @@
 package com.hxb.kotlin_demo
 
+import com.hxb.kotlin_demo.bean.Person
+import com.hxb.kotlin_demo.bean.Student
+
 
 /**
  * 泛型参数T声明为out,则T只能出现在输出位置（函数的返回值）
@@ -17,27 +20,6 @@ interface Comparable<in T>{
 }
 
 
-interface Tag<T>{
-    fun getContent(): T
-    fun setContent(content: T)
-}
-
-
-fun genericTest1(tag: Tag<String>) {
-    val other: Tag<out Any> = tag
-
-    //编译出错误
-//    other.setContent(1)
-
-    other.getContent()
-}
-
-fun genericTest2(tag: Tag<Number>) {
-    val other: Tag<in Int> = tag
-    other.setContent(10)
-    other.getContent()
-}
-
 fun genericOutTest(strs: Source<String>) {
 
     val objs: Source<Any> = strs
@@ -51,3 +33,46 @@ fun genericInTest(x: Comparable<Number>) {
 }
 
 
+interface Tag<T>{
+    fun getContent(): T
+    fun setContent(content: T)
+}
+
+
+/**
+ * 类型投影 out
+ */
+fun typeProjectionsOut(tag: Tag<String>) {
+    val other: Tag<out Any> = tag
+
+    //编译出错误
+//    other.setContent(1)
+
+    other.getContent()
+}
+
+
+/**
+ * 类型投影 in
+ */
+fun typeProjectionsIn(tag: Tag<Number>) {
+    val other: Tag<in Int> = tag
+    other.setContent(10)
+    other.getContent()
+}
+
+
+/**
+ * 泛型上界约束
+ */
+class Room<P : Person>{
+    var resident: P? = null
+}
+
+/**
+ * 星投影
+ */
+fun genericConstraintsTest() {
+    val room: Room<Student> = Room()
+    room.resident = Student("eri", "x10")
+}
