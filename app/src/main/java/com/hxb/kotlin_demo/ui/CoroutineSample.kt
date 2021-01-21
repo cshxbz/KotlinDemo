3e778ec80f8556c216c2cd4c1a4c29f56d801edb
cshxbz <cshxbz@163.com>
2021-01-21 21:10:02 +0800

    commit

--- app/src/main/java/com/hxb/kotlin_demo/ui/CoroutineSample.kt
+++ app/src/main/java/com/hxb/kotlin_demo/ui/CoroutineSample.kt
@@ -8,22 +8,26 @@ class CoroutineSample {
         fun test() {
             GlobalScope.launch(Dispatchers.Main) {
                 println("start ! ${getThreadName()}")
-                execWork1()
-                println("end ! ${getThreadName()}")
+                val work1Result = execWork1()
+                println("end ! ${getThreadName()}--$work1Result")
             }
         }
 
-        private suspend fun execWork1() {
+        private suspend fun execWork1(): String {
+            var result: String
             withContext(Dispatchers.IO) {
                 println("work1 start ! ${getThreadName()}")
                 delay(3000L)
+//                Thread.sleep(3000L)
                 println("work1 finish ! ${getThreadName()}")
+                result = "apple"
             }
             println("execWork1 out finish ${getThreadName()}")
+            return result
         }
 
         private fun getThreadName(): String {
-            return "current thread name: ${Thread.currentThread().name}"
+            return Thread.currentThread().name
         }
 
     }
--- app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
+++ app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.hxb.kotlin_demo.ui
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.TextView
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.hxb.kotlin_demo.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,13 +16,13 @@ class MainActivity : AppCompatActivity() {
          * 导入 kotlinx.android.synthetic.main.＜布局＞.* 后，
          * 可以免去 findViewById, 可直接访问布局中的控件，使用控件的id来访问
          */
-        tv_text.setOnClickListener {
-            var v: TextView = it as TextView
-//            println("clicked ${(v as TextView).text}")
-
+        btn_exec_work.setOnClickListener {
             CoroutineSample.test()
         }
 
+        btn_click.setOnClickListener{
+            Log.i("System.out","click btn !")
+        }
 
 
     }
