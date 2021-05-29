0b7a7592d451d05219651cd1b83502f1c2698605
huangxiaobo <huangxiaobo@vmos.cn>
2021-05-29 13:01:13 +0800

    commit

--- app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
+++ app/src/main/java/com/hxb/kotlin_demo/ui/MainActivity.kt
@@ -1,28 +1,61 @@
 package com.hxb.kotlin_demo.ui
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import com.hxb.kotlin_demo.CoroutineSample
-import com.hxb.kotlin_demo.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hxb.kotlin_demo.databinding.ActivityMainBinding
+import com.hxb.kotlin_demo.launchCoroutines
+import com.hxb.kotlin_demo.utils.printlnLog
+import kotlinx.coroutines.*
+import java.lang.IndexOutOfBoundsException
+import java.lang.NullPointerException
+import java.net.SocketException
+import java.util.concurrent.TimeoutException
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
 
-        /*
-         * 导入 kotlinx.android.synthetic.main.＜布局＞.* 后，
-         * 可以免去 findViewById, 可直接访问布局中的控件，使用控件的id来访问
-         */
-        btn_exec_work.setOnClickListener {
-            CoroutineSample.test()
-        }
 
-        btn_click.setOnClickListener{
-            Log.i("System.out","click btn !")
+        binding.btnClick.setOnClickListener {
+
+
+            launchCoroutines(
+                block = {
+                    printlnLog("outer: ${Thread.currentThread().name}")
+
+                    GlobalScope.launch(Dispatchers.IO) {
+                        delay(2000)
+                        printlnLog("inner: ${Thread.currentThread().name}")
+                    }
+
+//                    withContext(Dispatchers.IO) {
+//                        delay(3000)
+//                    }
+
+
+                    printlnLog("outer: end")
+
+                },
+
+                onError = {
+                    printlnLog("onError")
+
+                },
+
+                onComplete = {
+                    printlnLog("onComplete")
+//                    throw IndexOutOfBoundsException()
+                }
+
+            )
+
+
         }
 
 
